f478d2a99ca1a51103c779313b4231d8cb03914e
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-06-13 18:14:28 +0700

    adjust story page

--- src/app/stories/[id]/layout.tsx
+++ src/app/stories/[id]/layout.tsx
@@ -3,17 +3,14 @@ import React from 'react'
 export default function layout({children}: {children: React.ReactNode}) {
   return (
     <div className='pt-16 md:grid grid-cols-3 gap-8 px-5 md:px-10 lg:px-20'>
-      <section className="col-span-2 flex flex-col gap-4 py-5">
+      <section className="col-span-2 flex flex-col gap-10 py-5">
         {children}
       </section>
-      <aside className='col-span-1 py-5'>
-        <div className='flex flex-col sticky top-24 gap-4'>
-          <div className='w-full h-[300px] md:h-[200px] lg:h-[250px] flex justify-center items-center bg-gray-400'>
+      <aside className='col-span-1 py-5 hidden md:block'>
+        <div className='flex flex-col md:sticky top-24 gap-4'>
+          <div className='flex justify-center items-center w-full md:h-[200px] lg:h-[250px] bg-gray-300'>
             <h4>Advertisement</h4>
           </div>
-          {/* <div className='w-full h-[300px] md:h-[200px] flex justify-center items-center bg-gray-400'>
-            <h4>Advertisement</h4>
-          </div> */}
         </div>
       </aside>
     </div>
--- src/app/stories/[id]/page.tsx
+++ src/app/stories/[id]/page.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import { staticStories } from '@/assets/data/static';
+import StoryCard from '@/components/StoryCard/StoryCard';
 import Image from 'next/image';
 import { useParams } from 'next/navigation';
 import React from 'react';
@@ -12,20 +13,33 @@ export default function page() {
 
   return (
     <>
-      <div className='flex flex-col gap-2'>
-        <h1 className='text-5xl font-bold'>{staticStories[storyId].title}</h1>
-        <p className='text-lg text-gray-700 italic'>{staticStories[storyId].shortDesc}</p>
+      <section className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-2'>
+          <h1 className='text-4xl md:text-5xl font-bold'>{staticStories[storyId].title}</h1>
+          <p className='text-lg text-gray-700 italic'>{staticStories[storyId].shortDesc}</p>
+        </div>
+        <Image
+          src={staticStories[storyId].imgUrl}
+          alt='story-img'
+          className='w-full h-full object-cover object-center'
+        />
+        <div className='flex flex-col gap-2'>
+          {staticStories[storyId].textContent.map((paragraph: string, index: number) => (
+            <p key={index} className='text-justify indent-10 md:indent-12'>{paragraph}</p>
+          ))}
+        </div>
+      </section>
+      <div className='md:hidden w-full h-[250px] flex justify-center items-center bg-gray-300'>
+        <h4>Advertisement</h4>
       </div>
-      <Image
-        src={staticStories[storyId].imgUrl}
-        alt='story-img'
-        className='w-full h-full object-cover object-center'
-      />
-      <div className='flex flex-col gap-2'>
-        {staticStories[storyId].textContent.map((paragraph: string, index: number) => (
-          <p key={index} className='text-justify indent-10 md:indent-16'>{paragraph}</p>
-        ))}
-      </div>
+      <section className='flex flex-col gap-4'>
+        <h3 className='text-xl font-bold'>More Sories</h3>
+        <div className="flex flex-col md:grid md:grid-cols-2 gap-4">
+          {staticStories.map((story) => (
+            <StoryCard key={story.id} story={story}/>
+          ))}
+        </div>
+      </section>
     </>
   )
 }
