18d425eb619268e2506853aed0c6953fece6c3db
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-06-15 16:33:32 +0700

    adjust login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 import standing3 from '@/assets/images/avatar/standing3.png';
+import LoginForm from '@/components/Forms/LoginForm/LoginForm';
 
 export default function Login() {
   return (
-    <div className='h-screen flex flex-row-reverse'>
+    <div className='md:min-h-screen flex flex-row-reverse'>
       <section className='flex-1 hidden md:flex justify-center items-center bg-orange-300'>
         <div className="w-[280px] md:w-[320px] lg:w-[350px] h-[280px] md:h-[320px] lg:h-[350px] bg-white rounded-full relative top-10 md:top-0 md:bottom-10 flex justify-center items-center">
           <h3 className='absolute md:-left-0 md:text-8xl lg:text-9xl font-extrabold w-1/3'>Log in.</h3>
@@ -17,21 +18,10 @@ export default function Login() {
           />
         </div>
       </section>
-      <section className='flex-1 flex justify-center items-center'>
-        <form className='flex flex-col gap-4 w-4/5 mx-auto'>
-          <h2 className='md:hidden text-3xl font-bold'>Login</h2>
-          <div className='flex flex-col gap-2'>
-            <div className='flex flex-col gap-1'>
-              <label htmlFor="email" className='font-medium'>Email:</label>
-              <input id='email' type="email" className='px-2 py-1 border' />
-            </div>
-            <div className='flex flex-col gap-1'>
-              <label htmlFor="password" className='font-medium'>Password:</label>
-              <input id='password' type="password" className='px-2 py-1 border' />
-            </div>
-          </div>
-          <button className='p-2 bg-green-500 text-white rounded-full'>Login</button>
-        </form>
+      <section className='flex-1 flex justify-center items-center py-20 md:py-0'>
+        <div className='w-4/5 mx-auto'>
+          <LoginForm/>
+        </div>
       </section>
     </div>
   )
--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,8 +1,8 @@
-'use client'
+'use client';
 
 import { notifyFailed, notifySuccess } from '@/helpers/toaster';
-import { loginFormSchema, signUpFormSchema } from '@/helpers/zodSchema';
-import { auth, db } from '@/libs/firebase';
+import { loginFormSchema } from '@/helpers/zodSchema';
+import { auth } from '@/libs/firebase';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
@@ -14,36 +14,40 @@ type FormData = z.infer<typeof loginFormSchema>;
 export default function LoginForm() {
   const { register, handleSubmit, reset, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(loginFormSchema) });
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const handleLoginUser = async (data: { email: string; password: string }) => {
+
+  const handleLoginUser = async (data: FormData) => {
     setIsSubmitting(true);
     try {
+      // Sign in with Firebase Auth
       await signInWithEmailAndPassword(auth, data.email, data.password);
       notifySuccess("User logged in successfully");
       reset();
     } catch (error) {
-      console.error("Error adding new user: ", error);
+      console.error("Error logging in: ", error);
       notifyFailed("Invalid email or password");
     } finally {
       setIsSubmitting(false);
-    };
+    }
   };
 
   return (
     <form className='flex flex-col gap-4' onSubmit={handleSubmit(handleLoginUser)}>
-      <h2 className='md:hidden text-3xl font-bold'>Sign Up</h2>
+      <h2 className='md:hidden text-3xl font-bold'>Login</h2>
       <div className='flex flex-col gap-2'>
         <div className='flex flex-col gap-1'>
           <label htmlFor="email" className='font-medium'>Email:</label>
-          <input id='email' type="email" className='px-2 py-1 border' {...register('email')}/>
+          <input id='email' type="email" className='px-2 py-1 border' {...register('email')} />
           {errors.email && <span className='text-xs text-red-600'>{errors.email.message}</span>}
         </div>
         <div className='flex flex-col gap-1'>
           <label htmlFor="password" className='font-medium'>Password:</label>
-          <input id='password' type="password" className='px-2 py-1 border' {...register('password')}/>
+          <input id='password' type="password" className='px-2 py-1 border' {...register('password')} />
           {errors.password && <span className='text-xs text-red-600'>{errors.password.message}</span>}
         </div>
       </div>
-      <button className={`${isSubmitting ? 'bg-gray-300' : 'bg-green-500'} p-2 text-white rounded-full`}>{isSubmitting ? 'Signing Up...' : 'Sign Up'}</button>
+      <button type="submit" className={`${isSubmitting ? 'bg-gray-300' : 'bg-black'} p-2 text-white rounded-full`} disabled={isSubmitting}>
+        {isSubmitting ? 'Logging In...' : 'Log In'}
+      </button>
     </form>
-  )
+  );
 }
