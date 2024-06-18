8f9b04198b827e3e3a14fa7bfbfc95c83485b1a6
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-06-18 11:47:53 +0700

    adjust bottom navbar

--- src/components/DashboardNavbar/BottomNavbar/BottomNavbar.tsx
+++ src/components/DashboardNavbar/BottomNavbar/BottomNavbar.tsx
@@ -23,7 +23,7 @@ export default function BottomNavbar() {
         <li className='w-full h-full'>
           <Link
             href={"/dashboard/for-you"}
-            className={`md:px-5 w-full h-full flex justify-center items-center text-2xl ${pathName === "/dashboard/for-you" || pathName === "/dashboard/following" ? 'font-bold border-t-2 border-blue-600'  : ''}`}
+            className="md:px-5 w-full h-full flex justify-center items-center text-2xl"
           >
             {pathName === "/dashboard/for-you" || pathName === "/dashboard/following" ?
               <IoHomeSharp/>
@@ -35,7 +35,7 @@ export default function BottomNavbar() {
         <li className='w-full h-full'>
           <Link
             href={"/dashboard/search"}
-            className={`md:px-5 w-full h-full flex justify-center items-center text-2xl ${pathName === "/dashboard/search" && 'font-bold border-t-2 border-blue-600'}`}
+            className="md:px-5 w-full h-full flex justify-center items-center text-2xl"
           >
             {pathName === "/dashboard/search" ?
               <IoSearch/>
@@ -47,7 +47,7 @@ export default function BottomNavbar() {
         <li className='w-full h-full'>
           <Link
             href={"/dashboard/write"}
-            className={`md:px-5 w-full h-full flex justify-center items-center text-2xl ${pathName === "/dashboard/write" && 'font-bold border-t-2 border-blue-600'}`}
+            className="md:px-5 w-full h-full flex justify-center items-center text-2xl"
           >
             <BsFeather/>
           </Link>
@@ -55,7 +55,7 @@ export default function BottomNavbar() {
         <li className='w-full h-full'>
           <Link
             href={"/dashboard/notifications"}
-            className={`md:px-5 w-full h-full flex justify-center items-center text-2xl ${pathName === "/dashboard/notifications" && 'font-bold border-t-2 border-blue-600'}`}
+            className="md:px-5 w-full h-full flex justify-center items-center text-2xl"
           >
             {pathName === "/dashboard/notifications" ?
               <IoNotifications/>
@@ -67,7 +67,7 @@ export default function BottomNavbar() {
         <li className='w-full h-full'>
           <Link
             href={"/dashboard/profile"}
-            className={`md:px-5 w-full h-full flex justify-center items-center text-2xl ${pathName === "/dashboard/profile" && 'font-bold border-t-2 border-blue-600'}`}
+            className="md:px-5 w-full h-full flex justify-center items-center text-2xl"
           >
             <span className='w-[30px] h-[30px] bg-gray-100 rounded-full border-2 border-gray-200'>
             </span>
