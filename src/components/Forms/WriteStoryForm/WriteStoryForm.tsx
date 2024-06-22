189dee4843090c0274e236914b056df092c39be2
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-06-22 10:29:13 +0700

    adjust write story form

--- src/components/Forms/WriteStoryForm/WriteStoryForm.tsx
+++ src/components/Forms/WriteStoryForm/WriteStoryForm.tsx
@@ -4,19 +4,19 @@ export default function WriteStoryForm() {
   return (
     <form className='flex flex-col gap-4'>
       <div className='flex flex-col gap-1'>
-        <label htmlFor="title" className='text-lg font-semibold'>Title</label>
+        <label htmlFor="title" className='font-medium'>Title</label>
         <input id='title' type="text" className='px-2 py-1 border rounded-md' />
       </div>
       <div className='flex flex-col gap-1'>
-        <label htmlFor="shortDesc" className='text-lg font-semibold'>Short Description</label>
+        <label htmlFor="shortDesc" className='font-medium'>Short Description</label>
         <input id='shortDesc' type="text" className='px-2 py-1 border rounded-md' />
       </div>
       <div className='flex flex-col gap-1'>
-        <label htmlFor="storyImage" className='text-lg font-semibold'>Story Image</label>
+        <label htmlFor="storyImage" className='font-medium'>Story Image</label>
         <input id='storyImage' type="file" className='px-2 py-1 border rounded-md' />
       </div>
       <div className='flex flex-col gap-1'>
-        <label htmlFor="textContent" className='text-lg font-semibold'>Text Content</label>
+        <label htmlFor="textContent" className='font-medium'>Text Content</label>
         <textarea
           name="textContent"
           id="textContent"
